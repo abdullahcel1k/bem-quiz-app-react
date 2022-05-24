bc67e08539ad82d5c449950bfcffad5776f87476
Abdullah Çelik <abdullahcelkk@gmail.com>
2022-05-24 16:42:02 +0300

    fix

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   const createCards = () => {
 
     return cards.map(item => {
-      cards += <div className="col-4 card" style="width: 18rem;">
+      return <div className="col-4 card">
         <img src="/images/quiz.webp" className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
@@ -18,16 +18,7 @@ const Home = () => {
 
   return (
     <div className='row'>
-      {cards.map(item => {
-        return <div className="col-4 card" >
-          <img src="/images/quiz.webp" className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>;
-      })}
+      {createCards()}
     </div>
   )
 }
